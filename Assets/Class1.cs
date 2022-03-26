b36d5b3ed7bb60d476b050b81cb25cc16fc87fbe
JEONJINJEONJIN <86611207+JEONJINJEONJIN@users.noreply.github.com>
2022-03-26 21:51:47 +0900

    3/26 9:51

---
+++ Assets/Class1.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Class1 : MonoBehaviour
+{
+    class Artist
+    {
+        //능력치
+        public int vocal;
+        public int dance;
+        public int appearance;
+        //능력 한계치
+        public int vocal_limit;
+        public int dance_limit;
+        public int appearance_limit;
+        //특성
+        public boolean homosexuality = false;
+        public boolean feminism = true;
+        public int patience;
+
+    }
+}
